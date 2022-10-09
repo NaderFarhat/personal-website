6a74eb939527c458ff8760ab80645b783ae464f7
Nader Farhat <nader.farhat@wises.com.br>
2022-10-09 16:33:53 -0300

    Fix index card project

--- components/CardProject.tsx
+++ components/CardProject.tsx
@@ -38,9 +38,12 @@ export default function CardProject({
           <p className="text-gray-700 text-sm">{description}</p>
         </div>
         <div className="px-6 pt-4 pb-2">
-          {tags.map((item) => {
+          {tags.map((item, index) => {
             return (
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              <span
+                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                key={index}
+              >
                 {item}
               </span>
             );
