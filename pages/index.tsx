e090bd24c7ca1bbd2592694b75407ded11899697
Nader Farhat <nader.farhat@wises.com.br>
2022-10-09 16:33:08 -0300

    Fix &apos;

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,9 @@ const Home: NextPage = () => {
   return (
     <div className="max-w-screen-md mx-auto h-4/5 flex justify-start items-center">
       <section className="w-full md:w-9/12 xl:w-8/12">
-        <span className="font-bold uppercase tracking-widest">Hey, I`m</span>
+        <span className="font-bold uppercase tracking-widest">
+          Hey, I&apos;m
+        </span>
         <h1 className="text-3xl lg:text-5xl font-bold text-red-600">
           Nader
           <br />
