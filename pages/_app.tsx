11f0b4347f39ec44b011d01544a4e0d216ba1f12
Nader Farhat <nader.farhat@wises.com.br>
2022-10-09 16:40:00 -0300

    Fix app type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "../components/navbar";
 
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: any) {
   return (
     <ThemeProvider attribute="class">
       <div className=" transition-opacity duration-1000 ease-out opacity-20 hover:opacity-100 bg-transparent h-screen">
